f6dc0ddb82c0278be9d03e6fe7e73229cd45879b
Zakaria Lachkar <97468839+LACHKAR99ZAKARIA@users.noreply.github.com>
2024-08-24 21:37:50 +0100

    int project

---
+++ Netflix clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Netflix clone
+//
+//  Created by Zakaria Lachkar on 24/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Netflix clone/Netflix_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_cloneApp.swift
+//  Netflix clone
+//
+//  Created by Zakaria Lachkar on 24/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
